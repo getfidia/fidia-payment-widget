e8e7a8426547c8cda690ea924185eeef08b15358
Gbahdeyboh <gbahdeybohbello@gmail.com>
2021-10-02 12:54:44 +0100

    Created a layout structure for the injection of iframes into the creators website

--- v1.js
+++ v1.js
@@ -1,3 +1,10 @@
-function test(params) {
-    console.log(test);
-}
+(function(){
+    const d = document;
+    const s = d.createElement("iframe");
+
+    s.src = "./index.html";
+    s.frameborder = 0;
+    s.style = "position: fixed; top: 0px; left: 0px; z-index: 2147483647; border: none; opacity: 1; width: 100%; height: 100%;";
+    s.name = "Fidia Widget";
+    d.getElementsByTagName("body")[0].appendChild(s);
+})()
