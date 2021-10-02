4b8475d8fe55324730ec890aacbc2727e0739184
Gbahdeyboh <gbahdeybohbello@gmail.com>
2021-10-02 23:26:17 +0100

    Moved files into their respective folders

--- js/v1.js
+++ js/v1.js
@@ -3,6 +3,7 @@
     const s = d.createElement("iframe");
 
     s.src = "./index.html";
+    // s.src = "http://embed.getfidia.com/";
     s.frameborder = 0;
     s.style = "position: fixed; top: 0px; left: 0px; z-index: 2147483647; border: none; opacity: 1; width: 100%; height: 100%;";
     s.name = "Fidia Widget";
