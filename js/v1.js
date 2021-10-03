09ed32905c523bdf543e5fcec23b56d57829fb92
Gbahdeyboh <gbahdeybohbello@gmail.com>
2021-10-03 18:51:46 +0100

    Implemented the UI of the embed payment links in vue

--- js/v1.js
+++ js/v1.js
@@ -5,6 +5,7 @@
     s.src = "./index.html";
     // s.src = "http://embed.getfidia.com/";
     s.frameborder = 0;
+    s.allowtransparency= true;
     s.style = "position: fixed; top: 0px; left: 0px; z-index: 2147483647; border: none; opacity: 1; width: 100%; height: 100%;";
     s.name = "Fidia Widget";
     d.getElementsByTagName("body")[0].appendChild(s);
