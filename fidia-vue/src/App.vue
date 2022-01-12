d026d19e12d3585b4a0a1b9a463952f5994c5fe9
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-12 13:12:06 +0100

    BUG :: unable target iframe  not openeing FEAT ::   passed data (slug, username and custom text) from the component prop to <script> ...</script.

--- fidia-vue/src/App.vue
+++ fidia-vue/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <button
+  <div>
+    <!-- <button
     class="fidia-embed-target"
     data-fidia-username="opeolluwa"
     data-fidia-slug="Opeolluwa"
   >
     ooo
-  </button>
+  </button> -->
+    <fidia-vue slug="Opeolluwa" text="buy me a coffee" username="opeolluwa" background="green" />
+  </div>
 </template>
 
 
 <script>
+import FidiaVue from "./components/FidiaVue.vue";
 export default {
-  mounted() {
+  components: { FidiaVue },
+  /*  mounted() {
     const fidiaScript = document.createElement("script");
     fidiaScript.async = true;
     fidiaScript.setAttribute(
@@ -19,6 +24,10 @@ export default {
       "https://embed.getfidia.com/js/payment-embed.js"
     );
     document.getElementsByTagName("head")[0].appendChild(fidiaScript);
+  }, */
+
+  component: {
+    FidiaVue,
   },
 };
 </script>
