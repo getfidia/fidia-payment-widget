9cb0a6ffdf6e554d8a39509c8104887be2b5482c
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-03-02 23:25:11 +0100

    FIXES ::

--- fidia-vue/src/fidia-vue.js
+++ fidia-vue/src/fidia-vue.js
@@ -8,48 +8,42 @@ Vue.directive('fidia', {
         //get the data bindings on the directive and set the element innerText to the provided text value
         const userData = binding.value;
 
+
         el.onclick = () => {
             const d = document;
+            // const w = window;
             const f = d.createElement("iframe");
-            const w = window;
-
 
             f.src = "https://embed.getfidia.com/payment.html";
-            // f.setAttribute("src", "http://127.0.0.1:3000/src/payment.html")
+            // f.src = "../payment.html";
             f.frameborder = 0;
             f.allowtransparency = true;
-            f.style = "display:block; position: fixed; top: 0px; left: 0px; z-index: 2147483647; border: none; opacity: 1; width: 100%; height: 100%;";
+            f.style = "display:none; position: fixed; top: 0px; left: 0px; z-index: 2147483647; border: none; opacity: 1; width: 100%; height: 100%;";
             f.name = "Fidia Widget";
             f.id = "fidia-embed-iframe";
             d.getElementsByTagName("body")[0].appendChild(f);
 
 
-            //parse the user data obtained from v-fidia directive binding
-            const { name: fidiaUsername, slug: fidiaSlug } = userData
-            const fidiaEmbedData = JSON.stringify({
-                fidiaUsername,
-                fidiaSlug,
-            });
+                //parse the user data obtained from v-fidia directive binding
+                const { name: fidiaUsername, slug: fidiaSlug } = userData
+                const fidiaEmbedData = JSON.stringify({
+                    fidiaUsername,
+                    fidiaSlug,
+                });
 
-            //for debugging alone
-            console.log(fidiaEmbedData);
 
-            // Child window and parent window can only communicate over events
-            const fidiaIframe = d.querySelector("#fidia-embed-iframe");
-            fidiaIframe.contentWindow.postMessage(fidiaEmbedData, '*');
+                const fidiaIframe = d.querySelector("#fidia-embed-iframe");
+                setTimeout(() => {
+                    fidiaIframe.style.display = "block";
+                }, 30);
 
-            w.onmessage = (e) => {
-                if (e.data == 'closeFidiaIframe') {
-                    // Close the iframe when this event is emitted to the parent document,
-                    const fidiaIframe = document.querySelector('#fidia-embed-iframe');
-                    fidiaIframe.style.display = 'none';
-                }
-            }
-
+                // Child window and parent window can only communicate over events
+                fidiaIframe.contentWindow.postMessage(fidiaEmbedData, "*");
+            // });
         }
-
     }
 })
+
 
 
 
