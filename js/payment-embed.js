f01977e6731caa36d41a05052327edd6e664b140
realabdullah <abdulodesanmi@gmail.com>
2022-09-20 23:10:19 +0100

    chore: payment link update for widget

--- js/payment-embed.js
+++ js/payment-embed.js
@@ -27,19 +27,45 @@
 	let templateFidiaButton = d.querySelectorAll(".fidia-embed-target");
 	templateFidiaButton = Array.prototype.slice.call(templateFidiaButton);
 
+	const buttonLabels = ["pay me", "book me", "donate", "support me"];
+	let buttonColor = "#ffffff";
+	let backgroundColor = "#292a73";
+
 	// Replace template buttons with fidia button(s)
 	for (let i = 0; i < templateFidiaButton.length; i++) {
+		// create a new button to embed
+		const newFidiaButton = d.createElement("button");
+
 		const currentButton = templateFidiaButton[i];
 		const currentButtonParent = currentButton.parentNode;
 
-		// create a new button to embed
-		const newFidiaButton = d.createElement("button");
+		newFidiaButton.setAttribute("data-fidia-label", currentButton.getAttribute("data-fidia-label"));
+
+		newFidiaButton.setAttribute("fidia-button-color", currentButton.getAttribute("fidia-button-color"));
+		newFidiaButton.setAttribute("fidia-button-background", currentButton.getAttribute("fidia-button-background"));
+
+		if (currentButton.getAttribute("fidia-button-background")) {
+			backgroundColor = currentButton.getAttribute("fidia-button-background");
+		}
+		
+		if (currentButton.getAttribute("fidia-button-color")) {
+			buttonColor = currentButton.getAttribute("fidia-button-color");
+		}
+
+		newFidiaButton.style.color = buttonColor;
+		newFidiaButton.style.backgroundColor = backgroundColor;
+
+		let buttonLabel = newFidiaButton.getAttribute("data-fidia-label");
+		if (!buttonLabels.includes(buttonLabel.toLocaleLowerCase())) {
+			buttonLabel = "Support Me";
+		}
+
 		newFidiaButton.innerHTML = `
             <svg style="padding-right: 10px;" width="26" height="29" viewBox="0 0 27 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M13.3333 9.99992C14.6519 9.99992 15.9408 10.3909 17.0371 11.1235C18.1335 11.856 18.9879 12.8972 19.4925 14.1154C19.9971 15.3335 20.1291 16.674 19.8719 17.9672C19.6147 19.2604 18.9797 20.4483 18.0474 21.3806C17.115 22.313 15.9271 22.9479 14.6339 23.2052C13.3407 23.4624 12.0003 23.3304 10.7821 22.8258C9.56393 22.3212 8.52274 21.4667 7.7902 20.3704C7.05766 19.2741 6.66666 17.9851 6.66666 16.6666C6.66867 14.8991 7.37169 13.2046 8.6215 11.9548C9.87131 10.7049 11.5658 10.0019 13.3333 9.99992ZM13.3333 3.33325C10.6962 3.33325 8.11838 4.11524 5.92573 5.58032C3.73307 7.04541 2.02411 9.12779 1.01494 11.5641C0.00576946 14.0005 -0.258275 16.6814 0.256195 19.2678C0.770664 21.8542 2.04054 24.23 3.90524 26.0947C5.76994 27.9594 8.14571 29.2293 10.7321 29.7437C13.3185 30.2582 15.9994 29.9941 18.4358 28.985C20.8721 27.9758 22.9545 26.2668 24.4196 24.0742C25.8847 21.8815 26.6667 19.3037 26.6667 16.6666C26.6667 13.1304 25.2619 9.73898 22.7614 7.23849C20.2609 4.73801 16.8696 3.33325 13.3333 3.33325Z" fill="white"/>
                 <path d="M13.3333 6.66667C15.1743 6.66667 16.6667 5.17428 16.6667 3.33333C16.6667 1.49238 15.1743 0 13.3333 0C11.4924 0 10 1.49238 10 3.33333C10 5.17428 11.4924 6.66667 13.3333 6.66667Z" fill="#F49C00"/>
             </svg>
-            Support Me
+            ${buttonLabel}
         `;
 		newFidiaButton.setAttribute("data-fidia-username", currentButton.getAttribute("data-fidia-username"));
 		newFidiaButton.setAttribute("data-fidia-slug", currentButton.getAttribute("data-fidia-slug"));
